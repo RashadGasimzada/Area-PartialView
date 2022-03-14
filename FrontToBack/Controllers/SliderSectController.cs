d61cd94818c6bc94722b9ade087f7cc1583c1458
Rashad Gasimzada <helloqwerty00@icloud.com>
2022-03-15 00:55:22 +0400

    Admin pannel and partial views created

---
+++ FrontToBack/Controllers/SliderSectController.cs
@@ -0,0 +1,25 @@
+﻿using FrontToBack.Data;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class SliderSectController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderSectController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.Where(m => m.IsDeleted == false).ToListAsync();
+            return View(sliders);
+        }
+    }
+}
